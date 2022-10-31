8790826a910450ca0f551bc5b5968041a8f61132
No_Purple <mambetalieva_ana@mail.ru>
2022-10-31 06:12:16 +0600

    Users

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -7,7 +7,7 @@ export const Success = ({ count }) => {
       <h3>Успешно!</h3>
       <p>Всем {count} пользователям отправлено приглашение.</p>
         <a href="/">
-            <button className="send-invite-btn">Назад</button>
+            <button className="send-invite-btn"> Назад</button>
         </a>
     </div>
   );
